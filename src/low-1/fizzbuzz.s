e5f45060239d2e8841ec01393a7cf12ee1a385d5
uta8a <noumodulity@gmail.com>
2020-06-29 21:47:44 +0900

    Infinite fizzbuzz

--- src/low-1/fizzbuzz.s
+++ src/low-1/fizzbuzz.s
@@ -2,17 +2,20 @@
 .global main
 
 main:
-  mov rdx, 0x8
+  mov rcx, 0x0
+  mov rdx, 0x9
   lea rsi, [fizzbuzz]
   mov rdi, 0x1
   mov rax, 0x1
   syscall
+  inc rcx
+  jmp main
   mov rax, 0x3c
   syscall
 
 fizzbuzz:
-  .ascii "fizzbuzz"
+  .ascii "fizzbuzz\n"
 fizz:
-  .ascii "fizz"
+  .ascii "fizz\n"
 buzz:
-  .ascii "buzz"
+  .ascii "buzz\n"
