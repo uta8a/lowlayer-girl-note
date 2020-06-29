0e9959cd0faa654f2012de91846d3b8e55c8b5cd
uta8a <noumodulity@gmail.com>
2020-06-29 21:28:08 +0900

    print fizzbuzz

--- src/low-1/fizzbuzz.s
+++ src/low-1/fizzbuzz.s
@@ -1,5 +1,18 @@
 .intel_syntax noprefix
 .global main
+
 main:
+  mov rdx, 0x8
+  lea rsi, [fizzbuzz]
+  mov rdi, 0x1
+  mov rax, 0x1
+  syscall
   mov rax, 0x3c
   syscall
+
+fizzbuzz:
+  .ascii "fizzbuzz"
+fizz:
+  .ascii "fizz"
+buzz:
+  .ascii "buzz"
