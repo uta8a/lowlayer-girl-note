a5154dca78ff912c78a38947ee3b25ab6c6a92da
uta8a <noumodulity@gmail.com>
2020-06-30 02:03:32 +0900

    misc

---
+++ src/low-1/exit-zero.s
@@ -0,0 +1,19 @@
+	.file	"exit-zero.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, %edi
+	call	exit@PLT
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 10.1.0"
+	.section	.note.GNU-stack,"",@progbits
